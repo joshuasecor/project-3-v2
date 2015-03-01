a4481a7a22a4480bb8a862ade5ee2266707188f9
wcebron <wcebron@gmail.com>
2015-02-28 20:35:47 -0600

    updated view templates

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
 
   resources :trainers
 
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
