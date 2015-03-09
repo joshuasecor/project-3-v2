57162eb9d6783b899c1c663f2949b9b1ce48b0c9
joshuasecor <joshuasecor@gmail.com>
2015-03-09 14:58:46 -0700

    Fixed an issue with the migration files not loading properly

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150309033206) do
+ActiveRecord::Schema.define(version: 20150309205119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
