83061b02dd2b15e084192e7ea2cd035a02b926c1
wcebron <wcebron@gmail.com>
2015-03-14 19:00:54 -0700

    hero image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,19 +28,8 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  config.action_mailer.default_url_options = { host: "https://radiant-temple-7904.herokuapp.com/"}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:            'smtp.sendgrid.net',
-  port:               587,
-  domain:             'https://radiant-temple-7904.herokuapp.com/',
-  user_name:          ENV["SENDGRID_USERNAME"],
-  password:           ENV["SENDGRID_PASSWORD"],
-  authentication:     'plain',
-  enable_starttls_auto: true  }
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -87,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = { host: "https://radiant-temple-7904.herokuapp.com/"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:            'smtp.sendgrid.net',
+  port:               587,
+  domain:             'https://radiant-temple-7904.herokuapp.com/',
+  user_name:          ENV["SENDGRID_USERNAME"],
+  password:           ENV["SENDGRID_PASSWORD"],
+  authentication:     'plain',
+  enable_starttls_auto: true  }
+  
 end
